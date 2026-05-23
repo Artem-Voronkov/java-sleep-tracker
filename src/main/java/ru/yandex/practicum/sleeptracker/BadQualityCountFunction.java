60b22e868353fe4d66aca79d3ee4a56c40f20a1e
Artem <malibu2903@yandex.ru>
2026-05-23 18:08:17 +0300

    Исправление 1

--- src/main/java/ru/yandex/practicum/sleeptracker/BadQualityCountFunction.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/BadQualityCountFunction.java
@@ -6,7 +6,7 @@ public class BadQualityCountFunction implements SleepAnalysisFunction {
     @Override
     public SleepAnalysisResult apply(List<SleepingSession> sessions) {
         long count = sessions.stream()
-                .filter(session -> session.getQuality() == SleepQuality.BAD )
+                .filter(session -> session.getQuality() == SleepQuality.BAD)
                 .count();
         return new SleepAnalysisResult("Сессия с плохим качеством сна(в минутах)", count);
     }
