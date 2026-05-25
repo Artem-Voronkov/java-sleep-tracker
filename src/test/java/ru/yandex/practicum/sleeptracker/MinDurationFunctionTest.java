bb23bff2751fb824155d6b3119d6f0573ed1ae48
Artem <malibu2903@yandex.ru>
2026-05-25 11:15:20 +0300

    Исправил 1

--- src/test/java/ru/yandex/practicum/sleeptracker/MinDurationFunctionTest.java
+++ src/test/java/ru/yandex/practicum/sleeptracker/MinDurationFunctionTest.java
@@ -11,7 +11,6 @@ class MinDurationFunctionTest {
 
     @Test
     void testMinWithMultipleSessions() {
-        // Given: сессии с длительностями 60, 120 и 30 минут → минимум = 30
         List<SleepingSession> sessions = List.of(
                 new SleepingSession(SleepQuality.GOOD,
                         LocalDateTime.of(2025, 10, 1, 22, 0),
@@ -25,29 +24,23 @@ class MinDurationFunctionTest {
         );
         MinDurationFunction function = new MinDurationFunction();
 
-        // When
         SleepAnalysisResult result = function.apply(sessions);
 
-        // Then
         assertEquals("Минимальная продолжительность сессии сна(в минутах): 30", result.toString());
     }
 
     @Test
     void testMinEmptyList() {
-        // Given: пустой список сессий
         List<SleepingSession> emptySessions = List.of();
         MinDurationFunction function = new MinDurationFunction();
 
-        // When
         SleepAnalysisResult result = function.apply(emptySessions);
 
-        // Then: для пустого списка возвращаем 0 или специальное сообщение
         assertEquals("Минимальная продолжительность сессии сна(в минутах): 0", result.toString());
     }
 
     @Test
     void testMinSingleSession() {
-        // Given: одна сессия длительностью 90 минут
         List<SleepingSession> singleSession = List.of(
                 new SleepingSession(SleepQuality.GOOD,
                         LocalDateTime.of(2025, 10, 1, 23, 0),
@@ -55,10 +48,8 @@ class MinDurationFunctionTest {
         );
         MinDurationFunction function = new MinDurationFunction();
 
-        // When
         SleepAnalysisResult result = function.apply(singleSession);
 
-        // Then
         assertEquals("Минимальная продолжительность сессии сна(в минутах): 90", result.toString());
     }
 }
